6e2190e38d90f0ab7ea26c809b2a66a3499c4049
Aleex87 <alessandrodanteabbate@gmail.com>
2025-11-28 23:42:45 +0100

    load sakila database and created the file load for loading the data in dlt

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 In this lab I will do data analysis on Sakila database
 
 TODO higlite from the lab 
-TODO 
+TODO description about the lab 
+TODO nice images/screenschot
+
+
+I have start with the creation of a data folder where peste the sqlite-sakila.db from kagle.
+Created the python file load_sakila_sqlite_duckdb for loading the data.
+Then I started the virtual envirment with uv init
+I have add ipykernel and dlt[sql_dtatbase]'
+Writed code in load-sakilasqlite for load the data 
---
+++ load_sakila_sqlite_duckdb.py
@@ -0,0 +1,32 @@
+import os
+import dlt
+from dlt.sources.sql_database import sql_database
+from pathlib import Path
+
+# --- Path to the file ---
+BASE_PATH = Path(__file__).parent
+DATA_PATH = BASE_PATH / "data"
+
+SQLITE_PATH = DATA_PATH / "sqlite-sakila.db"
+DUCKDB_PATH = DATA_PATH / "sakila.duckdb"
+
+# --- Config destination in duckdb
+os.environ["DESTINATION__DUCKDB__CREDENTIALS__DATABASE"] = str(DUCKDB_PATH)
+
+# --- the source 
+source = sql_database(
+    credentials=f"sqlite:///{SQLITE_PATH}",
+    schema="main"   
+)
+
+# --- Create a pipeline ---
+pipeline = dlt.pipeline(
+    pipeline_name="sakila_sqlite_duckdb",
+    destination="duckdb",     
+    dataset_name="staging"    
+)
+
+# --- Execution loadin all in duckdb ---
+load_info = pipeline.run(source, write_disposition="replace")
+
+print(load_info)
